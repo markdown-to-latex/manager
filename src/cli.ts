43bb5bb77485c513a1054fdc0711d4956e6bab9e
Toliak <toliakpurple@gmail.com>
2021-10-05 01:27:47 +0300

    Created manager
    - CLI utility for initializing a MD to LaTeX project
    - COvered with tests

---
+++ src/cli.ts
@@ -0,0 +1,36 @@
+import * as ts from 'typescript';
+import { newProjectEntrypoint } from './cli/newProject';
+import { getError, getHelp } from './cli/information';
+
+function parseAndExecuteArguments(): void {
+    if (ts.sys.args.length === 0) {
+        console.error(getError());
+        ts.sys.exit(1);
+    }
+    if (ts.sys.args[0] === 'init') {
+        newProjectEntrypoint(ts.sys.args.slice(1))
+            .then(() =>
+                console.log(
+                    '\x1b[32mMarkDown To LaTeX feature ' +
+                    'has been set up successfully\x1b[0m',
+                ),
+            )
+            .catch(err => {
+                console.error(err);
+            });
+        return;
+    }
+    if (
+        ts.sys.args[0] === '--help' ||
+        ts.sys.args[0] === '?' ||
+        ts.sys.args[0] === '-h'
+    ) {
+        console.error(getHelp());
+        return;
+    }
+
+    console.error(getError());
+    ts.sys.exit(1);
+}
+
+parseAndExecuteArguments();
---
+++ src/cli/utils.ts
@@ -0,0 +1,26 @@
+// See https://geedew.com/remove-a-directory-that-is-not-empty-in-nodejs/
+import * as fs from "fs";
+
+export function deleteFolderSyncRecursive(path: string): void {
+    if (fs.existsSync(path)) {
+        fs.readdirSync(path).forEach(function (file) {
+            const curPath = path + '/' + file;
+            if (fs.lstatSync(curPath).isDirectory()) {
+                // recurse
+                deleteFolderSyncRecursive(curPath);
+            } else {
+                // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(path);
+    }
+}
+
+export function camelToKebabCase(str: string): string {
+    if (!str) {
+        return '';
+    }
+    str = str[0].toLowerCase() + str.slice(1);
+    return str.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`);
+}
